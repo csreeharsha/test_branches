5cba4e7a34e3e42a9f794bfe6ee610f937a5b299
harsha <csreeharsha@gmail.com>
2016-09-12 18:09:45 +0530

    Initial commit of Maven Project to Dummy Branch which will be deleted later

---
+++ TestProject/src/main/java/com/sample/main/TestProject/App.java
@@ -0,0 +1,14 @@
+package com.sample.main.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("From TestProject Branch 1 which will be loaded and put to master again");
+    }
+}
