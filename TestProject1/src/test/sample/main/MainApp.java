a4d080e9937470007f73c3d856e8499b41b27d57
csreeharsha <csreeharsha@gmail.com>
2017-11-06 16:24:58 +0530

    commit from branch nov6

--- TestProject1/src/test/sample/main/MainApp.java
+++ TestProject1/src/test/sample/main/MainApp.java
@@ -5,6 +5,7 @@ public class MainApp {
 	public static void main(String[] args) {
 System.out.println("Hello World");
 System.out.println("Commit 1");
+System.out.println("Commit from child branch test_branch_nov_06");
 	}
 
 }
